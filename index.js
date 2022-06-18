2bb20ebfa892e8b37ed8fcbbfd939caf12f10d6d
mark harrison <darthmalick@gmail.com>
2022-06-18 15:23:59 -0500

    commit tree 25d9c0f886aad0f71149ed67da5e6f0f6bf75f49

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record){
+    const foundSeason = record.find(season => season.result === "W")
+   return foundSeason ? foundSeason.year : undefined
+}
